8724cbbc4d391437edd76a3aa0dbc700709a0999
Sheetal Gupta <70136254+sheetal-gupta-11@users.noreply.github.com>
2021-06-24 11:53:50 +0530

    Add files via upload

---
+++ tic tac toe gui.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 23:16:39 2019
+
+@author: HP
+"""
+from tkinter import *
+def ttt(buttons):
+    global bclick
+    if buttons(text)
+root=Tk()
+
+button1=Button(root,padx=25,pady=25
+               )
+button1.grid(row=1,column=0)
+button2=Button(root,padx=25,pady=25)
+button2.grid(row=1,column=1)
+button3=Button(root,padx=25,pady=25)
+button3.grid(row=1,column=2)
+button4=Button(root,padx=25,pady=25)
+button4.grid(row=2,column=0)
+button5=Button(root,padx=25,pady=25)
+button5.grid(row=2,column=1)
+button6=Button(root,padx=25,pady=25)
+button6.grid(row=2,column=2)
+button7=Button(root,padx=25,pady=25)
+button7.grid(row=3,column=0)
+button8=Button(root,padx=25,pady=25)
+button8.grid(row=3,column=1)
+button9=Button(root,padx=25,pady=25)
+button9.grid(row=3,column=2)
+
+root.mainloop()
